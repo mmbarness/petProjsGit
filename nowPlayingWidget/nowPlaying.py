b15428ae3efbb0e94c8644d3f94eb6677011c670
matthew barnes <011mbarnes@gmail.com>
2021-05-31 20:14:08 -0400

    changes

--- nowPlayingWidget/nowPlaying.py
+++ nowPlayingWidget/nowPlaying.py
@@ -3,30 +3,41 @@ from plexapi.server import PlexServer
 from plexwebsocket import PlexWebsocket, SIGNAL_CONNECTION_STATE
 import json 
 
+#"pip install asyncio, plexwebsocket"
+
+# local_client obtained by printing "plex.sessions.players in this file, then running it"
+local_client = "<PlexClient:/resources:xxxxxxxxx"
+json_file = "./NowPlayingInfo.json"
 baseurl = "<plex server address>"
 token = "<ur token here>"
 plex = PlexServer(baseurl, token)
-local_client = "<PlexClient:/resources:xxxxxxxxx" #obtained by printing "plex.sessions.players in this file, then running it"
 
-def print_info(msgtype, data, error):
+def session_actions(msgtype, data, error):
     sessions = plex.sessions()
-    num_sessions = (len(sessions))
+    numSessions = (len(sessions))
     for i in sessions:
         client = str(i.players[0])
         if client == local_client:
-            music_writer(i, num_sessions)
-
-def music_writer(session, num_sessions):
+            music_writer(i, numSessions)
+    if sessions == []: 
+        null_writer()
+        
+def music_writer(session, numSessions):
     info = {"Track Title": session.title,
                   "Album": session.album().title,
                   "Artist": session.artist().title, 
-                  "Sessions": num_sessions
+                  "Sessions": numSessions
                   }
-    with open('<json filepath>', 'w') as outfile:
+    with open(json_file, 'w') as outfile:
+        json.dump(info, outfile)
+    
+def null_writer():
+    info = {"Nothing Playing": ""}
+    with open(json_file, 'w') as outfile:
         json.dump(info, outfile)
 
 async def main():
-    ws = PlexWebsocket(plex, print_info, subscriptions=["playing"])
+    ws = PlexWebsocket(plex, session_actions, subscriptions=["playing"])
     await ws.listen()
 
 if __name__ == '__main__':
